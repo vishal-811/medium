cf32c828ff7f55d7b7157b5073a4f9ba5f37c69b
vishalsharma <vishalssharma811@gmail.com>
2024-05-20 00:18:57 +0530

    auth done

--- backend/src/routes/user.ts
+++ backend/src/routes/user.ts
@@ -1,13 +1,65 @@
 import { Hono } from "hono";
+import { PrismaClient } from '@prisma/client/edge'
+import { withAccelerate } from "@prisma/extension-accelerate"
 
-const userRouter =new Hono();
+const userRouter =new Hono<{
+      Bindings:{
+         DATABASE_URL:string
+      }
+}>();
 
 userRouter.post('/signup',async(c)=>{
-    return c.json({msg:"user signup sucessfully"});
+       const prisma =new PrismaClient({
+           datasourceUrl:c.env?.DATABASE_URL 
+       }).$extends(withAccelerate());
+        try {
+            const body =await c.req.json();
+              const alreadyexist = await prisma.user.findUnique({
+                   where:{
+                      email:body.email
+                   }
+              })
+              if(alreadyexist){
+                  c.status(409)
+                  return c.json({msg:"User already exist with this credentials"})
+              }
+            const user =await prisma.user.create({
+                 data:{
+                    username:body.username,
+                    email:body.email,
+                    name:body.name,
+                    password:body.password
+                 }
+            })
+           return c.json({msg:"user signup sucessfully"}); 
+        } catch (error) {
+             c.status(403)
+             return c.json({msg:"Error While Signup"})
+        }
 })
 
 userRouter.post('/signin',async(c)=>{
-    return c.json({msg:"user signin successfully"});
+       const prisma =new PrismaClient({
+            datasourceUrl:c.env?.DATABASE_URL
+       }).$extends(withAccelerate())
+       
+       try {
+        const body = await c.req.json();
+        const user = await prisma.user.findUnique({
+             where:{
+                 email:body.email,
+                 password:body.password
+             }
+        })
+        if(!user){
+            c.status(400)
+            return c.json({msg:"User doesn't exist with this credentials,Please signup"}) 
+        }
+        return c.json({msg:"User signin successfully"})
+       } catch (error) {
+           console.error(error);
+            return c.json({msg:"Signin failed,something went wrong"})
+       }
 })
 
 export default userRouter;
